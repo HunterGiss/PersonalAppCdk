a1a63ac0df377e590df4edc653867658d7f1843c
Hunter Gissinger <huntergissinger@gmail.com>
2024-12-17 17:38:30 -0800

    Adding eslint and prettier

--- bin/app.ts
+++ bin/app.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import { WebAppCdkStack } from "../lib/stacks/web-app-stack";
+import * as cdk from 'aws-cdk-lib';
+import { WebAppCdkStack } from '../lib/stacks/web-app-stack';
 
 const app = new cdk.App();
-const env = { account: "533267137845", region: "us-east-2" };
-new WebAppCdkStack(app, "WebAppStack", {env});
+const env = { account: '533267137845', region: 'us-east-2' };
+new WebAppCdkStack(app, 'WebAppStack', { env });
